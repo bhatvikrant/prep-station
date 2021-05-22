b4efa33c4c9e7e9c0d9771a93795b71826bdea6f
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-23 01:47:49 +0530

    <HowWeCanAssistYou /> done roughly, needs design improvements

--- src/components/Home/HowWeCanAssistYou.tsx
+++ src/components/Home/HowWeCanAssistYou.tsx
@@ -1,40 +1,62 @@
 // REACT SLICK
 import Slider from 'react-slick'
 
+// DATA
+import { howWeCanAssistYouData } from './how-we-can-assist-you.data'
+
 const HowWeCanAssistYou: React.FC = () => {
   const settings = {
+    // dots: true,
+    // infinite: true,
+    // speed: 500,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    arrows: false,
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // arrows: false
+    slidesToShow: 5,
+    // slidesToScroll: 3
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
   return (
     <div>
-      <h2> Single Item</h2>
+      <div className="my-8 text-center">
+        <h2 className="font-semibold sm:text-4xl">How We Can Assist You</h2>
+      </div>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-          <div className="border-2 bg-red-300">
-            loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
+        {howWeCanAssistYouData.map((data, idx) => (
+          <div key={idx} className="p-4 border h-60">
+            <p className="text-2xl italic font-bold leading-snug">
+              {data.label}
+            </p>
           </div>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        ))}
       </Slider>
     </div>
   )
---
+++ src/components/Home/how-we-can-assist-you.data.ts
@@ -0,0 +1,12 @@
+export const howWeCanAssistYouData = [
+  { label: 'Know Your Chances To Get Into A College In Terms Of Your Profile' },
+  { label: 'Take An Aptitude Test To Evaluate Yourself' },
+  { label: 'Get Insider Information About Colleges' },
+  { label: 'Get All Your Doubts Cleared - Chat With Us' },
+  { label: 'Get Help In Choosing The Right Coaching Institute' },
+  { label: 'Analyse Which Test Series Would Be The Best For You' },
+  {
+    label:
+      'Know About Interview Experiences Of Students From Tier One Colleges',
+  },
+]
