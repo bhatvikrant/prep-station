567c6da8b6563438a894e2fbf528f4019a6f82c3
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-23 17:19:20 +0530

    Inter font restored

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;900&display=swap"
             rel="stylesheet"
-          /> */}
+          />
         </Head>
         <body>
           <Main />
