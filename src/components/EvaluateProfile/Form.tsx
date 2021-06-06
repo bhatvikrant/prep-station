34744b770fdef15b82587fee32157c7cf5ed664c
Vikrant Bhat <vik.bhat223@gmail.com>
2021-06-06 15:05:05 +0530

    form added (incomplete)

---
+++ src/components/EvaluateProfile/Form.tsx
@@ -0,0 +1,9 @@
+const Form: React.FC = () => {
+  return (
+    <div>
+      <h2 className="tw-section-heading">Evaluate</h2>
+    </div>
+  )
+}
+
+export default Form
--- src/pages/evaluate-profile.tsx
+++ src/pages/evaluate-profile.tsx
@@ -1,6 +1,7 @@
 // COMPONENTS
 import Landing from '@/components/EvaluateProfile/Landing'
 import ParaSection from '@/components/EvaluateProfile/ParaSection'
+import Form from '@/components/EvaluateProfile/Form'
 
 const EvaluateProfile: React.FC = () => {
   return (
@@ -8,17 +9,20 @@ const EvaluateProfile: React.FC = () => {
       <Landing />
 
       <div className="flex flex-col items-center m-4 my-10 space-y-20">
-        {/* ========= Profile Evaluation ========= */}
+        {/* ========= Section: Profile Evaluation ========= */}
         <ParaSection
           heading="Profile Evaluation"
           para={`While going for a master's degree in management, the most important factor to be considered is - Do you have what it takes to be a good manager? And that's where your profile comes into play. Having a good profile does not only put you ahead of others while applying for top business schools in the country but also, helps in reassuring that the career path that you've chosen for yourself is perfect for you. To know where you stand among lakhs of student applicants, the need for profile evaluation arises.`}
         />
 
-        {/* ========= How It Works ========= */}
+        {/* ========= Section: How It Works ========= */}
         <ParaSection
           heading="How It Works"
           para={`PrepStation's Profile Evaluator takes into account all the criteria that a particular business school considers while choosing students for its program. It carefully measures your grades and years of work experience against the amount of weightage given by top business schools in India, to these parameters. It analyses your background, stream, years of work experience and gives an accurate percentage chance of getting into each business school, separately. `}
         />
+
+        {/* ========= Form: Evaluate Profile  ========= */}
+        <Form />
       </div>
     </div>
   )
