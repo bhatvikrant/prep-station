f7a8d1d5bec1b645ee897557fa1241f7eb371df7
Vikrant Bhat <vik.bhat223@gmail.com>
2021-06-06 17:45:03 +0530

    firebase setup

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+}
+
+let app
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app()
+}
+
+const auth = app.auth()
+
+export { app, auth }
