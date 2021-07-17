1c8cacd25f35566f7cd6e0d3c2b1eea83399b71f
Vikrant Bhat <vik.bhat223@gmail.com>
2021-07-18 00:41:51 +0530

    evaluation form fixes

--- src/components/EvaluateProfile/Form.tsx
+++ src/components/EvaluateProfile/Form.tsx
@@ -1,4 +1,49 @@
+import { useState } from 'react'
+
+// TOAST
+import { errorNotification } from 'src/toast'
+
 const Form: React.FC = () => {
+	const [name, setName] = useState<string>('');
+	const [email, setEmail] = useState<string>('');
+	const [phoneNumber, setPhoneNumber] = useState<string>('');
+	const [tenthScore, setTenthScore] = useState<string>('');
+	const [twelfthStream, setTwelfthStream] = useState<string>('');
+	const [twelfthScore, setTwelfthScore] = useState<string>('');
+	const [gradStream, setGradStream] = useState<string>('');
+	const [gradScore, setGradScore] = useState<string>('');
+	const [workExp, setWorkExp] = useState<string>('');
+
+	const submit = () => {
+		if (!name) {
+			return errorNotification('Name is required')
+		}
+		if (!email) {
+			return errorNotification('Email is required')
+		}
+		if (!phoneNumber) {
+			return errorNotification('Phone Number is required')
+		}
+		if (!tenthScore) {
+			return errorNotification('10th score is required')
+		}
+		if (!twelfthStream) {
+			return errorNotification('12th stream is required')
+		}
+		if (!twelfthScore) {
+			return errorNotification('12th score is required')
+		}
+		if (!gradStream) {
+			return errorNotification('Grad stream is required')
+		}
+		if (!gradScore) {
+			return errorNotification('Grad score is required')
+		}
+		if (!workExp) {
+			return errorNotification('Work experience is required')
+		}
+	}
+
 	return (
 		<div className="w-full lg:max-w-lg">
 			<h2 className="mb-4 tw-section-heading">Evaluate</h2>
@@ -13,6 +58,9 @@ const Form: React.FC = () => {
 						type="text"
 						className="tw-form-input"
 						placeholder="Your full name"
+						value={name}
+						onChange={({ target: { value } }) => setName(value)}
+						required
 					/>
 				</div>
 				<div>
@@ -24,6 +72,9 @@ const Form: React.FC = () => {
 						type="email"
 						className="tw-form-input"
 						placeholder="Your email"
+						value={email}
+						onChange={({ target: { value } }) => setEmail(value)}
+						required
 					/>
 				</div>
 				<div>
@@ -35,6 +86,9 @@ const Form: React.FC = () => {
 						type="number"
 						className="tw-form-input"
 						placeholder="Your mobile number"
+						value={phoneNumber}
+						onChange={({ target: { value } }) => setPhoneNumber(value)}
+						required
 					/>
 				</div>
 				<div>
@@ -46,13 +100,20 @@ const Form: React.FC = () => {
 						type="number"
 						className="tw-form-input"
 						placeholder="Class 10th score"
+						value={tenthScore}
+						onChange={({ target: { value } }) => setTenthScore(value)}
+						required
 					/>
 				</div>
 				<div>
 					<label className="text-gray-700" htmlFor="12th-stream">
 						12th STREAM
 					</label>
-					<select name="12th-stream" id="12th-stream" className="tw-form-input">
+					<select name="12th-stream" id="12th-stream" className="tw-form-input"
+						value={twelfthStream}
+						onChange={({ target: { value } }) => setTwelfthStream(value)}
+						required
+					>
 						<option value="volvo">Volvo</option>
 						<option value="saab">Saab</option>
 						<option value="mercedes">Mercedes</option>
@@ -68,6 +129,9 @@ const Form: React.FC = () => {
 						type="number"
 						className="tw-form-input"
 						placeholder="Class 12th score"
+						value={twelfthScore}
+						onChange={({ target: { value } }) => setTwelfthScore(value)}
+						required
 					/>
 				</div>
 
@@ -75,7 +139,11 @@ const Form: React.FC = () => {
 					<label className="text-gray-700" htmlFor="grad-stream">
 						GRADUATION STREAM
 					</label>
-					<select name="grad-stream" id="grad-stream" className="tw-form-input">
+					<select name="grad-stream" id="grad-stream" className="tw-form-input"
+						value={gradStream}
+						onChange={({ target: { value } }) => setGradStream(value)}
+						required
+					>
 						<option value="volvo">Volvo</option>
 						<option value="saab">Saab</option>
 						<option value="mercedes">Mercedes</option>
@@ -92,6 +160,9 @@ const Form: React.FC = () => {
 						type="number"
 						className="tw-form-input"
 						placeholder="Graduation score"
+						value={gradScore}
+						onChange={({ target: { value } }) => setGradScore(value)}
+						required
 					/>
 				</div>
 
@@ -99,7 +170,11 @@ const Form: React.FC = () => {
 					<label className="text-gray-700" htmlFor="workExp">
 						WORK EXPERIENCE
 					</label>
-					<select name="workExp" id="workExp" className="tw-form-input">
+					<select name="workExp" id="workExp" className="tw-form-input"
+						value={workExp}
+						onChange={({ target: { value } }) => setWorkExp(value)}
+						required
+					>
 						<option value="volvo">Volvo</option>
 						<option value="saab">Saab</option>
 						<option value="mercedes">Mercedes</option>
@@ -107,7 +182,9 @@ const Form: React.FC = () => {
 					</select>
 				</div>
 
-				<button className="tw-primary-btn">Evaluate My Profile</button>
+				<button className="tw-primary-btn"
+					onClick={submit}
+				>Evaluate My Profile</button>
 			</div>
 		</div>
 	)
